dc227def1e77f70d673babc5ff2177b08770f0fe
digitalw8 <addiszemen@protonmail.com>
2024-03-27 18:49:51 -0400

    unit test coverage

---
+++ src/main/java/test/CalculatorTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtraction() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.subtract(5, 3));
+    }
+
+    @Test
+    public void testMultiplication() {
+        Calculator calculator = new Calculator();
+        assertEquals(15, calculator.multiply(5, 3));
+    }
+
+    @Test
+    public void testDivision() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.divide(10, 5));
+    }
+}
